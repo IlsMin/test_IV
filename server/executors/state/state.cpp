ea0e1e8fd94f03d60238e66f936988a1a50b43a9
IlsMin <mingaraev@yandex.ru>
2022-12-26 02:19:11 +0300

    Add files via upload

--- server/executors/state/state.cpp
+++ server/executors/state/state.cpp
@@ -3,38 +3,37 @@
 #include "common_parts.h"
 
 //bool State::equipmentState ( false);
-bool State::checkCommandArgs(CmdParts &cmnd)
+bool State::checkCommandArgs(CmdParts & cmnd)
 {
     bool rez = TestIV_ExecutorInterface::checkCommandArgs(cmnd);
     if (rez)
     {
-            rez = LED_STATES.contains(cmnd.arg.toString(), Qt::CaseInsensitive);
-        if(!rez)
+        rez = LED_STATES.contains(cmnd.arg.toString(), Qt::CaseInsensitive);
+        if (!rez)
             cmnd.rezult = T_FAILID + "wrong state";
     }
     return rez;
 }
 
-void State::doSetter(const CmdParts &cmnd)
+void State::doSetter(const CmdParts & cmnd)
 {
     equipmentState = isStateOn(cmnd);   // on : off
-    qDebug() <<"state" << cmnd.arg << equipmentState <<&equipmentState;
+    qDebug() << "state" << cmnd.arg << equipmentState << &equipmentState;
     QLCDNumber * numb = qobject_cast<QLCDNumber *>(pluginWidget);
-    if(numb != nullptr)
-       numb->setSegmentStyle( equipmentState ? QLCDNumber::Filled : QLCDNumber::Outline);
-    else
-        if( pluginWidget != nullptr)
-            pluginWidget->setVisible(equipmentState);
+    if (numb != nullptr)
+        numb->setSegmentStyle( equipmentState ? QLCDNumber::Filled : QLCDNumber::Outline);
+    else if ( pluginWidget != nullptr)
+        pluginWidget->setVisible(equipmentState);
 }
 
-void State::doGetter(CmdParts &cmnd)
+void State::doGetter(CmdParts & cmnd)
 {
-    cmnd.rezult = equipmentState;
+    cmnd.rezult = equipmentState ? "on" : "off";
 }
 
 bool State::getEquipmentState()
 {
-    qDebug() << "get_equipmentState"<< &State::equipmentState ;
-     return  State::equipmentState;
+    qDebug() << "get_equipmentState" << &State::equipmentState ;
+    return  State::equipmentState;
 
 }
