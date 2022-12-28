89f4301f406cf3462df83d4d663c9e7e296ac481
IlsMin <mingaraev@yandex.ru>
2022-12-28 13:40:25 +0300

    The equipment state was returned

--- server/executors/color/color.cpp
+++ server/executors/color/color.cpp
@@ -2,32 +2,32 @@
 #include "color.h"
 
 
-bool Color::checkCommandArgs(CmdParts &cmnd)
+bool Color::checkCommandArgs(CmdParts & cmnd)
 {
     bool rez = TestIV_ExecutorInterface::checkCommandArgs(cmnd);
-    if (rez)
+    if ( rez)
     {
-            rez = LED_COLORS.contains(cmnd.arg.toString(), Qt::CaseInsensitive);
-        if(!rez)
-            cmnd.rezult = T_FAILID+ "wrong color";
+        rez = LED_COLORS.contains(cmnd.arg.toString(), Qt::CaseInsensitive);
+        if (!rez)
+            cmnd.rezult = T_FAILID + "wrong color";
     }
     return rez;
 }
 
-void Color::doSetter(const CmdParts &cmnd)
+void Color::doSetter(const CmdParts & cmnd)
 {
-   Q_ASSERT(pluginWidget != nullptr);
-   pluginWidget->setStyleSheet("color: " + cmnd.arg.toString());
-   QLCDNumber * numb = qobject_cast<QLCDNumber *>(pluginWidget);
-   if(numb != nullptr)
-      numb->setSegmentStyle(QLCDNumber::Filled);
+    Q_ASSERT(pluginWidget != nullptr);
+    pluginWidget->setStyleSheet("color: " + cmnd.arg.toString());
+    QLCDNumber * numb = qobject_cast<QLCDNumber *>(pluginWidget);
+    if (numb != nullptr)
+        numb->setSegmentStyle(QLCDNumber::Filled);
 }
 
-void Color::doGetter(CmdParts &cmnd)
+void Color::doGetter(CmdParts & cmnd)
 {
     Q_ASSERT(pluginWidget != nullptr);
 
     QPalette palette = pluginWidget->palette();
     QColor clr =  palette.color(palette.WindowText);
-    cmnd.rezult = clr.name(); //  real color or sent value?
+    cmnd.arg = clr.name();  //  real color or sent value?
 }
