c9c3683c956a816a2b14cc78d4fc9b6c5868fcf2
IlsMin <mingaraev@yandex.ru>
2023-01-11 23:19:27 +0300

    rate setter (checking of  none digit values)

--- server/executors/rate/rate.cpp
+++ server/executors/rate/rate.cpp
@@ -11,54 +11,63 @@ bool rate::checkCommandArgs(CmdParts & cmnd)
     bool rez = TestIV_ExecutorInterface::checkCommandArgs(cmnd);
     if (rez)
     {
-        int herz = cmnd.arg.toInt();
-        rez = herz >= 0 && herz < 6;
-        if(!rez)
+        //  if (! cmnd.arg.canConvert<int>())
+        //    rez = false;
+        //else
         {
-            cmnd.rezult = T_FAILID+ "bad rate";
+            int herz = cmnd.arg.toInt();
+            rez = herz >= 0 && herz < 6;
+            if (herz == 0 && cmnd.arg != 0)
+                rez = false;
         }
+        if (!rez)
+            cmnd.rezult = T_FAILID + "bad rate";
     }
     return rez;
 }
 
-void rate::doSetter(const CmdParts &cmnd)
+void rate::doSetter(const CmdParts & cmnd)
 {
     timer.stop();
     QLCDNumber * numb = qobject_cast<QLCDNumber *>(pluginWidget);
-    if(nullptr == numb)
+    if (nullptr == numb)
     {
-       qCritical() << "LcdNumber NOT initializes!!!";
-       return;
+        qCritical() << "LcdNumber NOT initializes!!!";
+        return;
     }
 
-   int herz = cmnd.arg.toInt();
-   if(herz >= 0)
-   {
+    int herz = cmnd.arg.toInt();
+    if (herz >= 0)
+    {
         numb->display(herz);
 
-        if(herz > 0) {
-            timer.setInterval(1000/herz);
+        if (herz > 0)
+        {
+            timer.setInterval(1000 / herz);
             timer.start();
         }
-       // else //to emulate state = off OR ?
-
-   }
+        else
+        {
+            if (!show)  // make visible!
+                onTimer();
+        }
+    }
 }
 
-void rate::doGetter(CmdParts &cmnd)
+void rate::doGetter(CmdParts & cmnd)
 {
     QLCDNumber * numb = qobject_cast<QLCDNumber *>(pluginWidget);
-    cmnd.rezult = (numb != nullptr) ? numb->intValue(): 0;
+    cmnd.arg = (numb != nullptr) ? numb->intValue() : 0;
 }
 
 void rate::init()
 {
-    connect(&timer, &QTimer::timeout, this ,&rate::onTimer);
+    connect(&timer, &QTimer::timeout, this, &rate::onTimer);
 }
 
 void rate::onTimer()
 {
-   //qDebug() <<"timer!";
-   show = !show;
-   pluginWidget->setVisible(show);
+    //qDebug() <<"timer!";
+    show = !show;
+    pluginWidget->setVisible(show);
 }
